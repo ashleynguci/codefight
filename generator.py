e3b0dde0fc46e8cb32aaed23230a5dbb45469c9a
ashleynguci <trucanhftu2@gmail.com>
2019-03-10 23:12:16 +0200

    generator with fibonaxi

--- generator.py
+++ generator.py
@@ -10,3 +10,22 @@ def lottery():
 
 for random_number in lottery():
        print("And the next number is... %d!" %(random_number))
+       
+       
+def fibonaxi(num):
+    a,b = 0,1
+    for _ in range(num):
+        yield a
+        a,b = b, a+b
+        
+for i in fibonaxi(5):
+    print(i)
+    
+def factorial(num):
+    a,b = 1,2
+    for _ in range(num):
+        yield a
+        a,b = a*b,b+1
+        
+for i in factorial(5):
+    print(i)
